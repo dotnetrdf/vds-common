08b17067de8bc9877d43e0a1fb5433678590d5fe
rvesse <rvesse@dotnetrdf.org>
2012-11-28 13:58:48 -0800

    Bumped versions in AssemblyInfo.cs files

--- src/sl4-wp/Properties/AssemblyInfo.cs
+++ src/sl4-wp/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Resources;
@@ -33,5 +33,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
+
