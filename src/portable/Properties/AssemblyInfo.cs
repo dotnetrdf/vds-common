42cf5a2c0d87d3cbd3e383c696ef7a56db6cf3c5
rvesse <rvesse@dotnetrdf.org>
2013-01-14 08:25:14 -0800

    Add ProjectSync pre-build event to Portable Library build, incorporate Portable Library build in NAnt build scripts, add Portable Library build to solution

--- src/portable/Properties/AssemblyInfo.cs
+++ src/portable/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("VDS.Common")]
-[assembly: AssemblyDescription("Common data structures for .Net projects (.Net 4.0 Client Profile Build)")]
+[assembly: AssemblyDescription("Common data structures for .Net projects (.Net Portable Library Build)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Visual Design Studios")]
 [assembly: AssemblyProduct("VDS.Common")]
