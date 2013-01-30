0d77649c9484a719da7d9363e8c8a655686043e1
rvesse <rvesse@dotnetrdf.org>
2013-01-30 11:07:26 -0800

    Pull out all common logic from Trie into AbstractTrie

--- src/net40-client/Tries/Trie.cs
+++ src/net40-client/Tries/Trie.cs
@@ -26,35 +26,22 @@ using System.Linq;
 namespace VDS.Common.Tries
 {
     /// <summary>
-    /// Trie data structure which maps decomposable keys to values
+    /// Standard implementation of a Trie data structure
     /// </summary>
     /// <typeparam name="TKey">Type of keys</typeparam>
     /// <typeparam name="TKeyBit">Type of key bits</typeparam>
     /// <typeparam name="TValue">Type of values to map to</typeparam>
     /// <remarks>
-    /// <para>
-    /// Keys are converted into a sequence of key bits using a user provided function
-    /// </para>
-    /// <para>
-    /// Original code taken from <a href="http://code.google.com/p/typocalypse/source/browse/#hg/Trie">Typocolypse</a> but has been heavily rewritten to be much more generic and LINQ friendly
-    /// </para>
     /// </remarks>
     public class Trie<TKey, TKeyBit, TValue>
-        : ITrie<TKey, TKeyBit, TValue>
+        : AbstractTrie<TKey, TKeyBit, TValue>
         where TValue : class
-    {
-        private Func<TKey, IEnumerable<TKeyBit>> _keyMapper;
-        private ITrieNode<TKeyBit, TValue> _root;
-        
+    {   
         /// <summary>
         /// Create an empty trie with an empty root node.
         /// </summary>
         public Trie(Func<TKey, IEnumerable<TKeyBit>> keyMapper)
-        {
-            if (keyMapper == null) throw new ArgumentNullException("keyMapper", "Key Mapper function cannot be null");
-            this._keyMapper = keyMapper;
-            this._root = this.CreateNewChild(null, default(TKeyBit));
-        }
+            : base(keyMapper) { }
 
         /// <summary>
         /// Method which creates a new child node
@@ -62,192 +49,9 @@ namespace VDS.Common.Tries
         /// <param name="parent">Parent Node</param>
         /// <param name="key">Key Bit</param>
         /// <returns></returns>
-        protected virtual ITrieNode<TKeyBit, TValue> CreateNewChild(ITrieNode<TKeyBit, TValue> parent, TKeyBit key)
+        protected override ITrieNode<TKeyBit, TValue> CreateNewChild(ITrieNode<TKeyBit, TValue> parent, TKeyBit key)
         {
             return new TrieNode<TKeyBit, TValue>(null, key);
-        }
-
-        /// <summary>
-        /// Gets the Root Node of the Trie
-        /// </summary>
-        public ITrieNode<TKeyBit, TValue> Root
-        {
-            get
-            {
-                return this._root;
-            }
-        }
-
-        /// <summary>
-        /// Adds a new key value pair, overwriting the existing value if the given key is already in use
-        /// </summary>
-        /// <param name="key">Key to search for value by</param>
-        /// <param name="value">Value associated with key</param>
-        public void Add(TKey key, TValue value)
-        {
-            ITrieNode<TKeyBit, TValue> node = _root;
-            IEnumerable<TKeyBit> bs = this._keyMapper(key);
-            foreach (TKeyBit b in bs)
-            {
-                node = node.MoveToChild(b);
-            }
-            node.Value = value;
-        }
-
-        /// <summary>
-        /// Remove the value that a key leads to and any redundant nodes which result from this action
-        /// </summary>
-        /// <param name="key">Key of the value to remove</param>
-        public void Remove(TKey key)
-        {
-            ITrieNode<TKeyBit, TValue> node = this._root;
-            IEnumerable<TKeyBit> bs = this._keyMapper(key);
-            foreach (TKeyBit b in bs)
-            {
-                //Bail out early if the key doesn't go anywhere
-                if (!node.TryGetChild(b, out node)) return;
-            }
-            node.Value = null;
-
-            //Remove all ancestor nodes which don't lead to a value.
-            while (!node.IsRoot && !node.HasValue && node.Count == 0)
-            {
-                TKeyBit prevKey = node.KeyBit;
-                node = node.Parent;
-                node.RemoveChild(prevKey);
-            }
-        }
-
-        /// <summary>
-        /// Finds and returns a Node for the given Key
-        /// </summary>
-        /// <param name="key">Key</param>
-        /// <returns>Null if the Key does not map to a Node</returns>
-        public ITrieNode<TKeyBit, TValue> Find(TKey key)
-        {
-            return this.Find(key, this._keyMapper);
-        }
-
-        /// <summary>
-        /// Finds and returns a Node for the given Key using the given Key to Key Bit mapping function
-        /// </summary>
-        /// <param name="key">Key</param>
-        /// <param name="keyMapper">Function to map keys to key bits</param>
-        /// <returns>Null if the Key does not map to a Node</returns>
-        /// <remarks>
-        /// The ability to provide a custom mapping function allows you to do custom lookups into the Trie.  For example you might want to only match some portion of the key rather than the entire key
-        /// </remarks>
-        public ITrieNode<TKeyBit, TValue> Find(TKey key, Func<TKey, IEnumerable<TKeyBit>> keyMapper)
-        {
-            return this.Find(keyMapper(key));
-        }
-
-        /// <summary>
-        /// Finds and returns a Node for the given sequence of Key Bits
-        /// </summary>
-        /// <param name="bs">Key Bits</param>
-        /// <returns>Null if the Key does not map to a Node</returns>
-        /// <remarks>
-        /// The ability to provide a specific seqeunce of key bits may be useful for custom lookups where you don't necessarily have a value of the <strong>TKey</strong> type but do have values of the <strong>TKeyBit</strong> type
-        /// </remarks>
-        public ITrieNode<TKeyBit, TValue> Find(IEnumerable<TKeyBit> bs)
-        {
-            ITrieNode<TKeyBit, TValue> node = this._root;
-            foreach (TKeyBit b in bs)
-            {
-                //Bail out early if key does not exist
-                if (!node.TryGetChild(b, out node)) return null;
-            }
-            return node;
-        }
-
-        /// <summary>
-        /// Moves to the Node associated with the given Key creating new nodes if necessary
-        /// </summary>
-        /// <param name="key">Key</param>
-        /// <returns>Trie Node</returns>
-        public ITrieNode<TKeyBit, TValue> MoveToNode(TKey key)
-        {
-            ITrieNode<TKeyBit, TValue> node = _root;
-            IEnumerable<TKeyBit> bs = this._keyMapper(key);
-            foreach (TKeyBit b in bs)
-            {
-                node = node.MoveToChild(b);
-            }
-            return node;
-        }
-
-        /// <summary>
-        /// Gets/Sets the Value associated with a given Key
-        /// </summary>
-        /// <param name="key">Key</param>
-        /// <returns>Value associated with the given Key, may be null if no value is associated</returns>
-        /// <exception cref="KeyNotFoundException">Thrown if you try to get a value for a key that is not in the Trie</exception>
-        public TValue this[TKey key]
-        {
-            get
-            {
-                ITrieNode<TKeyBit, TValue> node = this.Find(key);
-                if (node == null) throw new KeyNotFoundException();
-                return node.Value;
-            }
-            set
-            {
-                this.Add(key, value);
-            }
-        }
-
-        /// <summary>
-        /// Tries to get the Value associated with a given Key
-        /// </summary>
-        /// <param name="key">Key</param>
-        /// <param name="value">Value</param>
-        /// <returns>True if the Key exists in the Trie, False if it does not</returns>
-        public bool TryGetValue(TKey key, out TValue value)
-        {
-            ITrieNode<TKeyBit, TValue> node = this._root;
-            IEnumerable<TKeyBit> bs = this._keyMapper(key);
-            foreach (TKeyBit b in bs)
-            {
-                //Bail out early if key does not exist
-                if (!node.TryGetChild(b, out node))
-                {
-                    value = null;
-                    return false;
-                }
-            }
-            value = node.Value;
-            return true;
-        }
-
-        /// <summary>
-        /// Gets the Count of all Nodes in the Trie
-        /// </summary>
-        public int Count
-        {
-            get
-            {
-                return this._root.CountAll;
-            }
-        }
-
-        /// <summary>
-        /// Gets all the Values in the Trie
-        /// </summary>
-        public IEnumerable<TValue> Values
-        {
-            get
-            {
-                return this._root.Values;
-            }
-        }
-
-        /// <summary>
-        /// Clears the Trie
-        /// </summary>
-        public void Clear()
-        {
-            this._root.Clear();
         }
     }
 }
