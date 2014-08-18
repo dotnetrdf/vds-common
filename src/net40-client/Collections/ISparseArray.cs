ec6fb170f30632b521be38cfc898a9a181337c3f
rvesse <rvesse@dotnetrdf.org>
2014-08-18 05:26:02 +0100

    Start implementing sparse arrays (Issue #5)

---
+++ src/net40-client/Collections/ISparseArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VDS.Common.Collections
+{
+    /// <summary>
+    /// Interface for sparse arrays which are memory efficient implementations of arrays
+    /// </summary>
+    /// <typeparam name="T">Value type</typeparam>
+    public interface ISparseArray<T>
+        : IEnumerable<T>
+    {
+        /// <summary>
+        /// Gets/Sets the element at the specified index
+        /// </summary>
+        /// <param name="index">Index</param>
+        /// <returns>Element at the given index</returns>
+        /// <exception cref="IndexOutOfRangeException">Thrown if the given index is out of range</exception>
+        T this[int index] { get; set; }
+
+        /// <summary>
+        /// Gets the length of the array
+        /// </summary>
+        int Length { get; }
+    }
+}
