03d09f4b08be96e43fa69b42253e94e4e9e132fe
rvesse <rvesse@dotnetrdf.org>
2015-05-29 12:17:05 +0100

    Add more BloomUtils and tests for these

--- src/net40-client/Filters/BloomUtils.cs
+++ src/net40-client/Filters/BloomUtils.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace VDS.Common.Filters
 {
@@ -30,7 +27,7 @@ namespace VDS.Common.Filters
         /// <param name="expectedItems">Expected number of items that will be added to the filter</param>
         /// <param name="errorRate">Desired error rate, the value given is treated as a 1 in N error rate</param>
         /// <returns>Bloom Filter parameters</returns>
-        public static IBloomFilterParameters CalculateBloomParameters(int expectedItems, int errorRate)
+        public static IBloomFilterParameters CalculateBloomParameters(long expectedItems, long errorRate)
         {
             if (expectedItems < 1) throw new ArgumentException("expectedItems must be >= 1", "expectedItems");
             if (errorRate < 1) throw new ArgumentException("errorRate must be >= 1", "errorRate");
@@ -43,7 +40,7 @@ namespace VDS.Common.Filters
         /// <param name="expectedItems">Expected number of items that will be added to the filter</param>
         /// <param name="errorRate">Desired error rate given as a value between 0 and 1.0</param>
         /// <returns>Bloom Filter parameters</returns>
-        public static IBloomFilterParameters CalculateBloomParameters(int expectedItems, double errorRate)
+        public static IBloomFilterParameters CalculateBloomParameters(long expectedItems, double errorRate)
         {
             if (expectedItems < 1) throw new ArgumentException("expectedItems must be >= 1", "expectedItems");
             if (errorRate < 0d || errorRate > 1d) throw new ArgumentException("errorRate must be in the range 0-1", "errorRate");
@@ -51,7 +48,33 @@ namespace VDS.Common.Filters
             double numBits = Math.Ceiling((expectedItems*Math.Log(errorRate))/Math.Log(1d/Math.Pow(2d, Math.Log(2))));
             double numHashFunctions = Math.Round(Math.Log(2d)*(numBits/expectedItems));
 
-            return new BloomFilterParameters(Convert.ToInt32(numBits), Convert.ToInt32(numHashFunctions));
+            try
+            {
+                return new BloomFilterParameters(Convert.ToInt32(numBits), Convert.ToInt32(numHashFunctions));
+            }
+            catch (OverflowException)
+            {
+                throw new ArgumentException(String.Format("The given parameters would result in a Bloom filter that required more than {0} bits/hash functions", Int32.MaxValue));
+            }
+        }
+
+        /*
+         ln p = -(m/n) * ((ln 2)^2).
+         */
+
+        /// <summary>
+        /// Given some parameters and the expected number of items calculates the error rate
+        /// </summary>
+        /// <param name="expectedItems">Expected number of items that will be added to the filter</param>
+        /// <param name="parameters">Bloom Filter Parameters</param>
+        /// <returns>Error Rate as a value between 0 and 1.0</returns>
+        public static double CalculateErrorRate(long expectedItems, IBloomFilterParameters parameters)
+        {
+            if (expectedItems < 1) throw new ArgumentException("expectedItems must be >= 1", "expectedItems");
+            if (parameters == null) throw new ArgumentNullException("parameters");
+
+            double lnP = (-1d*((double) parameters.NumberOfBits / expectedItems))*Math.Pow(Math.Log(2), 2d);
+            return Math.Pow(Math.E, lnP);
         }
     }
 }
--- test/Filters/BloomUtilsTests.cs
+++ test/Filters/BloomUtilsTests.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using NUnit.Framework;
 
 namespace VDS.Common.Filters
@@ -11,14 +8,41 @@ namespace VDS.Common.Filters
     {
         // Test cases are based on values calculated at http://hur.st/bloomfilter
 
+        private void CheckErrorRate(long expectedItems, long expectedErrorRate, IBloomFilterParameters parameters)
+        {
+            long actualErrorRate = CalculateErrorRate(expectedItems, parameters);
+            Console.WriteLine("n = {0}, p = 1 in {1}", expectedItems, actualErrorRate);
+            Assert.AreEqual(expectedErrorRate, actualErrorRate);
+
+            // If we half the number of items we add the error rate should decrease
+            // NB - Since we are expressing error rate as 1 in p actual value will increase
+            actualErrorRate = CalculateErrorRate(expectedItems/2, parameters);
+            Console.WriteLine("n = {0}, p = 1 in {1}", expectedItems / 2, actualErrorRate);
+            Assert.IsTrue(actualErrorRate > expectedErrorRate);
+
+            // If we double the number of items we add the error rate should increase
+            // NB - Since we are expressing error rate as 1 in p actual value will decrease
+            actualErrorRate = CalculateErrorRate(expectedItems * 2, parameters);
+            Console.WriteLine("n = {0}, p = 1 in {1}", expectedItems * 2, actualErrorRate);
+            Assert.IsTrue(actualErrorRate < expectedErrorRate);
+        }
+
+        private static long CalculateErrorRate(long expectedItems, IBloomFilterParameters parameters)
+        {
+            double calcErrorRate = BloomUtils.CalculateErrorRate(expectedItems, parameters);
+            return Convert.ToInt64(1/calcErrorRate);
+        }
+
         [TestCase(100000, 100000, 2396265, 17)]
         [TestCase(100000, 10000, 1917012, 13)]
         [TestCase(100000, 1000, 1437759, 10)]
-        public void CheckParameterCalculation(int expectedItems, int errorRate, int expectedNumBits, int expectedNumHashFunctions)
+        public void CheckParameterCalculation(long expectedItems, long errorRate, int expectedNumBits, int expectedNumHashFunctions)
         {
             IBloomFilterParameters parameters = BloomUtils.CalculateBloomParameters(expectedItems, errorRate);
             Assert.AreEqual(expectedNumBits, parameters.NumberOfBits);
             Assert.AreEqual(expectedNumHashFunctions, parameters.NumberOfHashFunctions);
+
+            CheckErrorRate(expectedItems, errorRate, parameters);
         }
     }
 }
