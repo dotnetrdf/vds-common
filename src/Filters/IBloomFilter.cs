5b7ab7864bc34056e1b26346d543a633e9a747f9
Brandon Thetford <brandon@dodecatec.com>
2023-05-20 07:13:22 -0700

    Use contravariance
    More explicit flexibility, especially if also implementing IComparable<T>, which is also contravariant since Framework 4.0, on derived types

--- src/Filters/IBloomFilter.cs
+++ src/Filters/IBloomFilter.cs
@@ -29,7 +29,7 @@ namespace VDS.Common.Filters
     /// <remarks>
     /// A bloom filter is a data structure that can be used to determine whether an item may have previously been seen in a memory efficient way that does not require storing the actual items.  The trade off is that it may yield false positives however it is guaranteed to never yield false negatives.  This makes it an ideal data structure for implementings things like distinctness where it doesn't matter if a few non-distinct items are discarded.
     /// </remarks>
-    public interface IBloomFilter<T> : IBloomFilterParameters
+    public interface IBloomFilter<in T> : IBloomFilterParameters
     {
         /// <summary>
         /// Adds an item to the filter
